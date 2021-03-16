4a64b7f72ddd22326099077bcf63cca367bee1e4
coder-web-development <80450304+coder-web-development@users.noreply.github.com>
2021-03-16 18:16:35 +0500

    new sites

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:false,
+		slidesToShow:1,
+		slidesToScroll:1,
+		speed:300,
+		easing:'linear',
+		infinite:false,
+		initialSlide:0,
+		autoplay:false,
+		autoplaySpeed:3000,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,
+		draggable:false,
+		swipe:false,
+		touchThreshold:5,
+		touchMove:true,
+		waitForAnimate:true,
+		centerMode:false,
+		variableWidth:false,
+		rows:1,
+		slidesPerRow:1,
+		vertical:false,
+		verticalSwiping:false,
+	});
+	$('.sliderbig').slick({
+		
+	})
+});
